b7073ba84d7d3cea067a4848a13dbe74031e7624
ASIT GARABADU <asitgarabadu1@gmail.com>
2025-04-18 19:51:19 +0530

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-#string concatenation(means just adding using the "+" sign)
-#where the input will executed first
-print("what is your name? "+input("your name")+"!")
-#another way of doing this code using variable
-hello = input("your name?")
-print("what is your name -"+hello+"!")
-
